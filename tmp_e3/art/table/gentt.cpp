d1af60a459fe21251c6c6e7c6aa9493e80c4a182
U-ZONK\oleg <you@example.com>
2018-01-23 16:02:59 +0400

    z

---
+++ tmp_e3/art/table/gentt.cpp
@@ -0,0 +1,5 @@
+// this program uses matrix (mx) format to generate TT
+// the output is .ttb which is exhaustive binary output
+
+// ! check that every input is present
+
---
+++ tmp_e3/art/table/ttattb.cpp
@@ -0,0 +1,4 @@
+// this program converts between .tta and .ttb formats 
+
+// ! check that output file is not present
+
